cba177a216526b2871b976675b9fcbbba0a3f745
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-07-28 17:37:12 +0200

    paragraphs in the response

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState } from 'react';
-import { ChatGPT } from '../components/ChatGpt'
+import ChatGPT from '../components/ChatGpt'
 import { Sidebar } from '../components/Sidebar'
 import styles from './page.module.css'
 
