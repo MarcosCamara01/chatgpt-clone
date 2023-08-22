2dea34587cffbc242ce0f53f85037a8d5dad1212
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-08-22 22:12:27 +0200

    code block and text rendering with custom styles

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { AiOutlineUser } from 'react-icons/ai';
 import { GptIcon } from "../helpers/icons";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import '../styles/css/messages.css';
+
+const customCodeStyle = {
+  padding: '16px',
+  backgroundColor: '#000',
+};
 
 export const Messages = ({ messages }) => {
     return (
         <>
             {messages.map((message) => {
                 const isChatGPT = message.role !== 'user';
-                const messageContent = message.content.split('\n\n');
+                const messageContent = message.content;
+                const messageParts = messageContent.split(/(```[\s\S]*?```)/);
 
                 return (
                     <div key={message.id} className={`bx-group ${isChatGPT ? "bx-clear" : "bx-dark"}`}>
@@ -19,9 +27,33 @@ export const Messages = ({ messages }) => {
                                 <div className='bx-svg'><AiOutlineUser /></div>
                             }
                             <div className={`messages-bx ${isChatGPT ? "message-gpt" : ""}`}>
-                                {messageContent.map((paragraph, index) => (
-                                    <p key={index}>{paragraph}</p>
-                                ))}
+                                {messageParts.map((part, index) => {
+                                    if (part.startsWith('```')) {
+                                        const codeBlockParts = part.split('\n');
+                                        const language = codeBlockParts[0].replace(/```/, '').trim();
+                                        console.log(language)
+                                        const codeContent = codeBlockParts.slice(1, -1).join('\n');
+
+                                        return (
+                                            <SyntaxHighlighter
+                                                key={index}
+                                                language={language}
+                                                style={atomDark}
+                                                customStyle={customCodeStyle}>
+                                                {codeContent}
+                                            </SyntaxHighlighter>
+                                        );
+                                    } else {
+                                        const paragraphs = part.split('\n\n');
+                                        return (
+                                            <div key={index}>
+                                                {paragraphs.map((paragraph, pIndex) => (
+                                                    <p key={pIndex}>{paragraph}</p>
+                                                ))}
+                                            </div>
+                                        );
+                                    }
+                                })}
                             </div>
                         </div>
                     </div>
