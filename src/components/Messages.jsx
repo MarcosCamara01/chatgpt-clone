223b3e3d8be59c69a57a5c8d29f071755db1ef94
Marcos Penelas <marcospenelascamara@gmail.com>
2024-03-03 15:40:34 +0000

    mobile bug in SyntaxHighlighter fixed

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -7,11 +7,12 @@ import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 const customCodeStyle = {
     padding: '16px',
     backgroundColor: '#000',
+    width: '100%'
 };
 
 export const Messages = ({ messages }) => {
     return (
-        <div className='mt-[65px]'>
+        <div className='mt-[65px] w-full'>
             {messages.map((message) => {
                 const isChatGPT = message.role !== 'user';
                 const messageContent = message.content;
@@ -22,7 +23,7 @@ export const Messages = ({ messages }) => {
                         key={message.id}
                         className={`w-full ${isChatGPT ? "bg-[#444654]" : "bg-[#343541]"}`}
                     >
-                        <div className='w-[90%] max-w-3xl py-6 mx-auto flex gap-6'>
+                        <div className='w-[90%] max-w-3xl py-6 mx-auto flex sm:gap-6 gap-3'>
                             {isChatGPT ?
                                 <div
                                     className='bg-[#19C37D] min-w-[30px] max-w-[30px] h-[30px] flex items-center justify-center rounded-sm'
@@ -36,7 +37,7 @@ export const Messages = ({ messages }) => {
                                     <AiOutlineUser className="text-2xl min-w-[24px] h-[24px] text-white" />
                                 </div>
                             }
-                            <div>
+                            <div className='lg:max-w-full max-w-[85%]'>
                                 {messageParts.map((part, index) => {
                                     if (part.startsWith('```')) {
                                         const codeBlockParts = part.split('\n');
